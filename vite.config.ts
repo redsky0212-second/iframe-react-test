df86c80685204f060b50f5df3ee5f83c15d34545
redsky0212 <wihyun0238@gmail.com>
2026-04-03 18:49:40 +0900

    사이트 루트 설정 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import { resolve } from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "src"),
-    },
-  },
-  server: {
-    port: 5179, // 원하는 포트로 설정
-  },
+	base: '/iframe/',
+	plugins: [react(), tailwindcss()],
+	resolve: {
+		alias: {
+			'@': resolve(__dirname, 'src'),
+		},
+	},
+	server: {
+		port: 5179, // 원하는 포트로 설정
+	},
 });
